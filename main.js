1f1a1137307cc2a1bfaa185e35d5e52d5faad2a5
Lucas Acosta <lucas.heck@live.com>
2023-10-03 11:15:49 -0300

    finished project

--- main.js
+++ main.js
@@ -10,5 +10,16 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDefault()
+
+        const url = $('#urlImg').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${url}"/>`).appendTo(novoItem)
+        $(`<div class="overlayImg">
+            <a href="${url}" target="_blank" title="Ver imagem em tamanho real"> Ver imagem em tamanho real </a>
+            </div>`).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+
+        $('#urlImg').val('')
     })
 })
